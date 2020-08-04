72b99c54282b8fc5d28a0e84a4b9049053d6e601
n0y0j <n0y0j77@gmail.com>
2020-08-04 15:28:13 +0900

    Setting the web of the shiritori using react.

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const posts = await res.json();
+      this.setState({
+        posts
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // 보여지는 부분
+  render() {
+    return (
+      <div>
+        {this.state.posts.map(item => (
+          <div key={item.id}>
+            <h1>{item.word}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
