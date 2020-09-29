bc06e3fd347d028af944a5c6481cfcad642081ed
n0y0j <n0y0j77@gmail.com>
2020-09-29 18:34:48 +0900

    feat: Implement web_crawlar using asyncio

--- shiritori_web/server/user/views.py
+++ shiritori_web/server/user/views.py
@@ -6,8 +6,8 @@ from .models import User
 from .serializers import UserSerializer
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import time
 import lxml
+import asyncio
 
 class ListUser(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -17,54 +17,59 @@ class DetailUser(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-@api_view(['POST'])
-def word(request) :
-    temp_mean = ''
-    mean = []
-    valid = False
-    Query_word = request.data
-    words = Query_word['word']
-
-    game_res = {
-        'word_mean': mean,
-        'valid': valid
-    }
-
+async def WebCrawling(word):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('disable-gpu')
     driver = webdriver.Chrome(
         '/home/n0y0j/workspace/jpn-shiritori-web-ver/shiritori_web/server/chromedriver', chrome_options=options)
-    driver.get('https://ja.dict.naver.com/#/search?query=' + words)
-    time.sleep(1)
+    driver.get('https://ja.dict.naver.com/#/search?query=' + word)
+    await asyncio.sleep(1)
 
     html = driver.page_source
     soup = BeautifulSoup(html, "lxml")
+    return soup
 
-    hotKeys = soup.select(
+def getMean(soup, word, valid):
+    mean_tag = ""
+    wordKeys = soup.select(
         "div.component_keyword.has-saving-function > div.row > div.origin > a.link > strong.highlight")
 
-    if (hotKeys != []):
-        for key in hotKeys:
+    if (wordKeys != []):
+        for key in wordKeys:
             check_text = key.get_text()
 
-        if (words == check_text):
+        if (word == check_text):
             valid = True
             mean_Keys = soup.select(
                 "div#searchPage_entry.section.section_keyword > div.component_keyword.has-saving-function > div.row > ul.mean_list > li.mean_item p.mean")
 
             for mKey in mean_Keys:
-                temp_mean += mKey.get_text()
+                mean_tag += mKey.get_text()
 
-        mean = temp_mean.replace('\t', '').split('\n\n')
+    return mean_tag, valid
 
-        for i in range(len(mean)):
-            mean[i]=mean[i].strip('\n ')
+
+@api_view(['POST'])
+def word(request) :
+    mean_tag = ''
+    mean = []
+    valid = False
+    Query_word = request.data
+    word = Query_word['word']
+    
+    soup = asyncio.run(WebCrawling(word))
+
+    mean_tag, valid = getMean(soup, word, valid)
+
+    mean = mean_tag.replace('\t', '').split('\n\n')
+    for i in range(len(mean)):
+        mean[i]=mean[i].strip('\n ')
             
-        game_res = {
-            'word_mean': mean,
-            'valid': valid
-        }
+    game_res = {
+        'word_mean': mean,
+        'valid': valid
+    }
 
     return JsonResponse(game_res)
 
